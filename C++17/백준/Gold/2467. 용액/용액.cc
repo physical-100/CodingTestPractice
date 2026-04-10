939e5dcbab5d7982cae799109a3b3e15edcaab62
Physical-100 <126838090+physical-100@users.noreply.github.com>
2026-04-10 11:01:00 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+산성은 10억 까지
+알칼리는 -10억 
+용액의 특성값의 합으로 정의 -> 같은 양의 두 용액을 혼합하여 특성값이 0에 가까운 용약 
+어쨌든 두개를 더해서 0에 가장 가까우면 된다
+값은 오름차순으로 주어진다 -> 정렬 불필요 
+*/
+const int INF = 2*1e9+1;
+int arr[100001];
+int n;
+pair<int,int> ans;
+int min_val = INF;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    for(int i=0;i<n;i++){
+        // 특성 값이 다 다르다. 
+        //0은 존재 하지 않음.
+        cin>> arr[i];
+    }
+    int r = n-1,l=0;
+    while(1){
+        int s = arr[r]+arr[l];
+        if(abs(s)<= min_val) {
+            min_val= abs(s);
+            ans= {l,r};
+        }
+        if(s==0) 
+            break;
+        else if(s>0){
+            r--;
+        }
+        else{
+            l++;
+        }
+        if(l>=r){
+            //cout<< l<<r<<'\n';
+            break;  
+        } 
+    }
+    //cout<< INF <<'\n';
+    cout<<arr[ans.first]<<' '<<arr[ans.second];
+    return 0;
+}
