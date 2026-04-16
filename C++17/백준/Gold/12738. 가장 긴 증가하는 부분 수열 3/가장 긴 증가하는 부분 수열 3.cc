05c3e0f1541bee95e3871219dec1a6bda748f4e5
Physical-100 <126838090+physical-100@users.noreply.github.com>
2026-04-16 16:01:47 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 172 ms, Memory: 22248 KB -BaekjoonHub

---
+++ C++17/백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+최대 1000,000이라서 NlogN인 이분탐색을 활용해야한다 
+
+lower_bound를 쓰면 아주 간단해짐 
+사용법:  lower_bound(lists.begin(),lists,end(), arr[i]);
+*/
+vector<long long> lists;
+int n;
+long long arr[1000001];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>> n;
+    for(int i=0;i<n;i++)cin>>arr[i];
+    lists.push_back(arr[0]);
+    for(int i=1;i<n;i++){
+        if(arr[i]>lists.back()) lists.push_back(arr[i]);
+        else{
+            auto it = lower_bound(lists.begin(),lists.end(),arr[i]);
+            *it = arr[i];
+        }
+    }
+    cout<< lists.size();
+    return 0;
+}
