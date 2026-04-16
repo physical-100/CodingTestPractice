1a9d1a14c7f2db3bd7b63106627d9d15fd348293
Physical-100 <126838090+physical-100@users.noreply.github.com>
2026-04-16 15:04:16 +0900

    [Silver I] Title: 카잉 달력, Time: 456 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int  t;
+/*
+*/
+int gcd(int a,int b){
+    if(a==0) return b;
+    return gcd(b%a,a);
+}
+int lcm(int a , int b){
+    return a/gcd(a,b)*b;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>t;
+    while(t--){
+        int m,n,x,y;
+        int ans =-1;
+        cin>> m>>n>>x>>y;
+        if(x==m) x=0;
+        if(y==n) y=0;
+        for(int i=x;i<=lcm(m,n);i+=m){
+            if(i==0) continue;
+            if(i%n==y){
+                ans=i;
+                break;
+            }
+        }
+        cout<<ans<<'\n';
+        
+    }
+    return 0;
+}
